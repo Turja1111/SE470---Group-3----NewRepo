535a4468ec03b75f4055d831259f7cdba689ce6c
raqin31 <mohammed.raqin.rahman@g.bracu.ac.bd>
2025-01-04 23:57:08 +0600

    feat: Implement decision logic to evaluate trainee's answers and determine companion eligibility

--- backend/controllers/All.js
+++ backend/controllers/All.js
@@ -324,4 +324,27 @@ export const getProfilePicture = async (req, res) => {
     }
 };
 
+export const companion = async (req, res) => {
+    console.log("Received request to update companion status");
+    try {
+        const id = req.user.id;
+        await User.findByIdAndUpdate(id, {
+            isCompanion: true
+        })
+        return res.status(200).json({ message: "Companion status updated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
 
+export const companions = async (req, res) => {
+    console.log("Received request to get companions");
+    try {
+        const companions = await User.find({ isCompanion: true });
+        res.status(200).json(companions);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
